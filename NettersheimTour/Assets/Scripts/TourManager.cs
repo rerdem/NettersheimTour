4050566a93409c36766565cf136aae5e700bdeea
fallenpenguin <ronaerdem@gmail.com>
2017-01-06 19:45:52 +0100

    fixed cardboard scene problem somehow, added popup before loading VR scene, back button doesn't work for some reason

--- NettersheimTour/Assets/Scripts/TourManager.cs
+++ NettersheimTour/Assets/Scripts/TourManager.cs
@@ -8,15 +8,21 @@ public class TourManager : MonoBehaviour {
 	public static TourManager instance = null;
 
 	public GameObject startPopup;
+	public GameObject stationPopup;
+	public GameObject stationPopupCancelButton;
+	public GameObject stationPopupStartButton;
 
 	[SerializeField] private Text testLabelText;
+	[SerializeField] private Text stationPopupText;
 
 	string qrString = "";
+
+	private string nextScene;
 
 	void Awake () {
 		if (instance == null) instance = this;
 		else if (instance != null) Destroy(gameObject);
-		DontDestroyOnLoad(gameObject);
+//		DontDestroyOnLoad(gameObject);
 	}
 
 	// Use this for initialization
@@ -26,13 +32,54 @@ public class TourManager : MonoBehaviour {
 		AndroidNFCReader.enableBackgroundScan ();
 		AndroidNFCReader.ScanNFC (gameObject.name, "OnFinishScan");
 
+		nextScene = "mainScreen";
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//		if (qrString == "tag1") {
+		//			SceneManager.LoadScene ("location1");
+		//		}
 		if (qrString == "tag1") {
-			SceneManager.LoadScene ("location1");
+			nextScene = "location1";
+			showPopup ();
 		}
+	}
+
+	void showPopup() {
+		if (nextScene == "location1") {
+			stationPopupText.text = "Dies ist der Info-Text zu Station 1";
+		}
+		else {
+			stationPopupText.text = "Kein Info-Text gefunden.";
+		}
+		stationPopup.SetActive (true);
+		stationPopupCancelButton.SetActive (true);
+		stationPopupCancelButton.GetComponent<Button> ().interactable = true;
+		stationPopupStartButton.SetActive (true);
+		stationPopupStartButton.GetComponent<Button> ().interactable = true;
+	}
+
+	public void hidePopup() {
+		stationPopup.SetActive (false);
+		stationPopupCancelButton.SetActive (false);
+		stationPopupCancelButton.GetComponent<Button> ().interactable = false;
+		stationPopupStartButton.SetActive (false);
+		stationPopupStartButton.GetComponent<Button> ().interactable = false;
+	}
+
+//	void OnLevelWasLoaded(int level) {
+//		startPopup = GameObject.Find ("startPopup");
+//		stationPopup = GameObject.Find ("stationPopup");
+//		stationPopupCancelButton = GameObject.Find ("stationPopupCancelButton");
+//		stationPopupStartButton = GameObject.Find ("stationPopupStartButton");
+//
+//		testLabelText = GameObject.Find ("testLabel").GetComponent<Text> ();
+//		stationPopupText = GameObject.Find ("stationPopupText").GetComponent<Text> ();
+//	}
+
+	public void loadLevel() {
+		SceneManager.LoadScene (nextScene);
 	}
 
 
